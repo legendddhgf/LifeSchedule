250f88bb777ca8fce046b16ee80f9bba03525f74
August Valera <4U6U57@gmail.com>
2017-11-26 22:47:16 -0800

    Started on commands with args library
    Example 2 in args documentation

--- LifeSchedule.cpp
+++ LifeSchedule.cpp
@@ -21,12 +21,23 @@
 int main(int argc, char** argv) {
 
   // Create parser
-  args::ArgumentParser parser("This is a test program.", "This goes after the options.");
-  args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+  args::ArgumentParser parser(
+      "A scheduler for human operating systems.",
+      "Project under development at http://github.com/legendddhgf/LifeSchedule"
+      );
+  args::Group commands(parser, "commands");
+  args::Command add(commands, "add", "add an activity to your life");
+  args::Command status(commands, "status", "see your life status");
+  args::HelpFlag help(parser, "help", "display this help menu", {'h', "help"});
 
   // Execute parser
   try {
     parser.ParseCLI(argc, argv);
+    if(add) {
+      std::cout << "Adding an activity" << std::endl;
+    } else if(status) {
+      std::cout << "Checking status" << std::endl;
+    }
   } catch(args::Help) {
     std::cout << parser;
     return 0;
